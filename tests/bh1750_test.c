df904ff94e069b30a5205852678f7c59a557e41a
Martin Prejsa <martinprejsa@duck.com>
2025-01-23 14:52:15 +0100

    bh1750.c tests

--- tests/bh1750_test.c
+++ tests/bh1750_test.c
@@ -1,8 +1,19 @@
 #include "bh1750.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main() {
 	bh1750_handle_t handle;
-	bh1750_init(&handle);
-	bh1750_set_mode(&handle, BH1750_MODE_CONT_HIGH_RES);
-	bh1750_read_measurement(&handle);
+	
+	if(!bh1750_init(&handle))
+	    exit(EXIT_FAILURE);
+	
+	if(!bh1750_set_mode(&handle, BH1750_MODE_ONE_TIME_HIGH_RES))
+	    exit(EXIT_FAILURE);
+
+	float m = bh1750_read_measurement(&handle);
+	if(m == -1)
+	    exit(EXIT_FAILURE);
+	printf("%.2f lux\n", m);
 }
